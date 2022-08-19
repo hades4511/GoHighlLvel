d8b8b6f7c5c9d158a5c0f89dc2947f4b486234b1
hades4511 <muhammadusman6242@gmail.com>
2022-08-20 02:09:58 +0500

    Added time to campaigns

--- calls.js
+++ calls.js
@@ -42,11 +42,32 @@ exports.createContact = async email => {
 
 exports.addContactToWorkflow = async (contactID, workflowID) => {
     const url = `${process.env.url}/contacts/${contactID}/workflow/${workflowID}`;
+    const toIsoString = date => {
+        const tzo = -date.getTimezoneOffset(),
+            dif = tzo >= 0 ? '+' : '-',
+            pad = function(num) {
+                return (num < 10 ? '0' : '') + num;
+            };
+      
+        return date.getFullYear() +
+            '-' + pad(date.getMonth() + 1) +
+            '-' + pad(date.getDate()) +
+            'T' + pad(date.getHours()) +
+            ':' + pad(date.getMinutes()) +
+            ':' + pad(date.getSeconds()) +
+            dif + pad(Math.floor(Math.abs(tzo) / 60)) +
+            ':' + pad(Math.abs(tzo) % 60);
+    }
+    const currentTime = new Date();
+    const updated = toIsoString(new Date(currentTime.getTime() + 1000 * 60));
+    console.log(currentTime);
+    console.log(updated);
     try{
         const response = await axios({
             method: 'post',
             headers: HEADERS,
             url: url,
+            data: {eventStartTime: updated}
         });
         return true
     }
--- index.js
+++ index.js
@@ -43,6 +43,31 @@ app.get('/get', (req, res, next) => {
     return addToWorkflow(req.query, res);
 });
 
+app.get('/get/time', (req, res, next) => {
+    const toIsoString = date => {
+        const tzo = -date.getTimezoneOffset(),
+            dif = tzo >= 0 ? '+' : '-',
+            pad = function(num) {
+                return (num < 10 ? '0' : '') + num;
+            };
+      
+        return date.getFullYear() +
+            '-' + pad(date.getMonth() + 1) +
+            '-' + pad(date.getDate()) +
+            'T' + pad(date.getHours()) +
+            ':' + pad(date.getMinutes()) +
+            ':' + pad(date.getSeconds()) +
+            dif + pad(Math.floor(Math.abs(tzo) / 60)) +
+            ':' + pad(Math.abs(tzo) % 60);
+    }
+    const currentTime = new Date();
+    const updated = toIsoString(new Date(currentTime.getTime() + 1000 * 60));
+    return res.json({
+        'time': currentTime,
+        'updatedTime': updated
+    });
+});
+
 app.post('/lead/post', (req, res, next) => {
     console.log('Lead POST URL');
     console.log(req.body.customData);
